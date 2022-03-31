893bfc52ea353bfcb054f32b04eb326047cdbf5e
drArancibiaDelgado <diegoarancibiadelgado53@gmail.com>
2022-03-31 14:12:04 -0230

    lo que uno ase

---
+++ principal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Mi nombre completo es : Diego Roberto Arancibia Delgado" << endl;
+	
+	cout << "Mi Ci es : 10387725" << endl;
+
+	cout << "Mi Cu es : 35-4980" << endl;
+
+	cout << "Mi carrera es : Ingenieria en sistemas" << endl;
+
+	cout << "Mi pasatiempo son : los deportes de contacto" << endl;
+
+    return 0;
+}
+

4b5f2edbbd64336cbd61c2d8b8496e85e57afc7d
drArancibiaDelgado <diegoarancibiadelgado53@gmail.com>
2022-03-31 17:13:41 -0230

    ultimos cambios en code

--- principal.cpp
+++ principal.cpp
@@ -4,22 +4,60 @@ using namespace std;
 
 int main()
 {
-	cout << "Mi nombre completo es : Diego Roberto Arancibia Delgado" << endl;
+	cout << "quiere ver mi brigrafia esciba si o no"<<endl;
+
+	string si, no;
+
+	cin >> si,no;
+
+	while(si == "si")
+	{
+		cout << "Mi nombre completo es : Diego Roberto Arancibia Delgado" << endl;
+
+		cout << "Mi Ci es : 10387725" << endl;
+
+		cout << "Mi Cu es : 35-4980" << endl;
+
+		cout << "Mi carrera es : Ingenieria en sistemas" << endl;
+
+		cout << "Mi pasatiempo son : los deportes de contacto, y arreglar computadoras" << endl;
+
+		cout << "Naci el 2000 el 5 de noviembre un domingo en la mañana" << endl;
+
+		cout << "Soy soltero actualmente" << endl;
+
+		cout << "Estuve en el colegio montesorri y en el Don Bosco" << endl;
+
+
+
+
+
+
+
+		cout << "\nquiere volver a ver la biografia" << endl;
+		cin >> si;
+		if (si == "si")
+		{
+			cout << "Mi nombre completo es : Diego Roberto Arancibia Delgado" << endl;
+
+			cout << "Mi Ci es : 10387725" << endl;
+
+			cout << "Mi Cu es : 35-4980" << endl;
+
+			cout << "Mi carrera es : Ingenieria en sistemas" << endl;
+
+			cout << "Mi pasatiempo son : los deportes de contacto, y arreglar computadoras" << endl;
+
+			cout << "Naci el 2000 el 5 de noviembre un domingo en la mañana" << endl;
+
+			cout << "Soy soltero actualmente" << endl;
+
+			cout << "Estuve en el colegio montesorri y en el Don Bosco" << endl;
+		}
+	}
 	
-	cout << "Mi Ci es : 10387725" << endl;
 
-	cout << "Mi Cu es : 35-4980" << endl;
+	return 0;
 
-	cout << "Mi carrera es : Ingenieria en sistemas" << endl;
-
-	cout << "Mi pasatiempo son : los deportes de contacto" << endl;
-
-	cout << "Naci el 2000 el 5 de noviembre un domingo en la mañana" << endl;
-
-	cout << "Soy soltero actualmente" << endl;
-
-	cout << "Estuve en el colegio montesorri y en el Don Bosco" << endl;
-
-    return 0;
 }
 
